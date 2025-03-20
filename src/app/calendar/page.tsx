869f50d300e1cc0ee66a5ec53a9486533d337922
devysi0827 <devysi0827@gmail.com>
2025-03-20 14:34:01 +0900

    [fix] calendar sever page params 비동기 처리

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,17 +1,18 @@
-import { fetchEvents } from "@/features/calendar/api/calendarApi";
+import { mockFetchMonthlyEvents } from "@/features/calendar/api/mockCalendarApi";
 
 import { ClientPage } from "./ClientPage";
 
 export default async function CalendarPage({
   searchParams,
 }: {
-  searchParams: { date?: string };
+  searchParams: Promise<{ [key: string]: string | undefined }>;
 }) {
-  const date = searchParams.date ? new Date(searchParams.date) : new Date();
+  const dateParam = (await searchParams).date;
+  const date = dateParam ? new Date(dateParam) : new Date();
 
   try {
-    const events = await fetchEvents(date);
-    // return <ClientPage initialEvents={events} initialDate={date} />;
+    const events = await mockFetchMonthlyEvents(date);
+    return <ClientPage initialEvents={events} />;
   } catch {
     throw new Error("캘린더 데이터를 불러오는 중 오류가 발생했습니다.");
   }
